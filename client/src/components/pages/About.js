f688f93c1d79b20e899018761a09716a7593d3f7
Akshansh <akshanshrewariya@gmail.com>
2021-06-08 01:32:26 +0530

    Prepare for deploy

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this App</h1>
+            <p className="my-1">Full sk app for contacts</p>
+            <p className="bg-dark p">Version</p> 1.0.0
+        </div>
+    );
+};
+
+export default About;
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
+
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
 
-const PORT = process.env.PORT || 5000;
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+}
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server started on the port ${PORT}`));
